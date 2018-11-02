494b22a66ecff29b6ef66f1831021bf5535e9056
Kacper Piątkowski <kacper.piatkowskii@gmail.com>
2018-11-02 11:10:35 +0100

    added case for 0 seconds ago

--- DateToolsSwift/DateTools/Date+TimeAgo.swift
+++ DateToolsSwift/DateTools/Date+TimeAgo.swift
@@ -114,9 +114,12 @@ public extension Date {
 //            return String.init(format: "%d min. temu", components.minute!)
             return String.init(format: "%d \(NSLocalizedString("time_few_minutes_ago", comment: ""))", components.minute!)
         }
-        else if (components.second! >= 0) {
+        else if (components.second! > 0) {
 //            return String.init(format: "%d sek. temu", components.second!)
             return String.init(format: "%d \(NSLocalizedString("time_few_seconds_ago", comment: ""))", components.second!)
+        }
+        else if (components.second! = 0) {
+            return NSLocalizedString("time_0_seconds_ago", comment: "")
         }
         else {
             return ""
